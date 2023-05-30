45099661ed9f9363ec870e3d4438116e7a1c668c
Nishant-Gupta <nishta.gupta@bd.com>
2023-05-30 11:39:32 +0530

    fixed id

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -21,5 +21,5 @@ gtag('config', 'G-FTH98G1LJR');
 
 // Google Tag Manager
 // eslint-disable-next-line
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PQFSS83');
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','G-FTH98G1LJR');
 
