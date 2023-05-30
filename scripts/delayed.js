f80b28be17655254e93da910d59849ffefcade8c
Nishant-Gupta <nishta.gupta@bd.com>
2023-05-30 11:38:32 +0530

    gtm

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,21 @@ import { sampleRUM } from './lib-franklin.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+// Global site tag (gtag.js) - Google Analytics
+
+// Initialize the data layer for Google Tag Manager
+// (this should mandatorily be done before the Cookie Solution is loaded)
+window.dataLayer = window.dataLayer || [];
+
+function gtag(...args) {
+  window.dataLayer.push(args);
+}
+
+gtag('js', new Date());
+
+gtag('config', 'G-FTH98G1LJR');
+
+// Google Tag Manager
+// eslint-disable-next-line
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PQFSS83');
+
