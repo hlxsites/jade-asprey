81ce1926cdd797d93bfad41204a7de662690f834
Nishant-Gupta <nishta.gupta@bd.com>
2023-05-30 22:29:07 +0530

    page view

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -20,6 +20,11 @@ function gtag(...args) {
   window.dataLayer.push(args);
 }
 
+async function pageViewDataPush() {
+  const pageData = await getPageEventData();
+  gtag(pageData);
+}
+
 gtag('js', new Date());
 
 gtag('config', gtmId);
@@ -28,4 +33,4 @@ gtag('config', gtmId);
 // eslint-disable-next-line
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer',gtmId);
 
-gtag('event', 'page_view', getPageEventData());
+pageViewDataPush();
